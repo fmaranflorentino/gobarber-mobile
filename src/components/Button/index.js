121d897b2da20182b3d56fd0a0a5d90b865bb950
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-04 22:49:45 -0300

    feat(pages/signIn): add page layout

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -12,7 +12,6 @@ export default function Button({children, loading, ...rest}) {
       ) : (
         <Text>{children}</Text>
       )}
-      <Text />
     </Container>
   );
 }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        style={{marginTop: 30}}
-        icon="call"
-        placeholder="Digite seu número"
-      />
+        <Form>
+          <FormInput
+            icon="main-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
 
-      <Button>Enviar</Button>
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+        </Form>
+
+        <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+
+        <SignLink>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
