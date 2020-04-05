af5d2f6b57983a8f31e6849314aaf90d89e8be9e
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-04 22:58:53 -0300

    feat(StatusBar): add StatusBar simple config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StatusBar} from 'react-native';
 
 import Routes from './routes';
 
 const App: () => React$Node = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
