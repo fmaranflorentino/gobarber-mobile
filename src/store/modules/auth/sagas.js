ec50b3121a0486fb7c431c5caf191a21af40d9f7
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-05 11:05:31 -0300

    feat(redux): config redux/persist/saga

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,84 @@
+import {Alert} from 'react-native';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
+import api from '~/services/api';
+
+import {signInSuccess, signFailure, signUpSuccess} from './actions';
+
+export function* signIn({payload}) {
+  try {
+    const {email, password} = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const {user} = response.data;
+
+    if (user.provider) {
+      Alert.alert(
+        'Erro no login',
+        'Usuário não pode ser prestador de serviços',
+      );
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${user.token}`;
+
+    yield put(signInSuccess(user.token, user));
+
+    // history.push('/dashboard');
+  } catch (error) {
+    Alert.alert('Falha na autenticação', 'verifique seus dados.');
+
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({payload}) {
+  try {
+    const {name, email, password} = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    Alert.alert(
+      'Cadastro realizado com sucesso! Agora você pode acessar sua conta.',
+    );
+
+    yield put(signUpSuccess());
+
+    // history.push('/');
+  } catch (error) {
+    Alert.alert('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({payload}) {
+  if (!payload) {
+    return;
+  }
+
+  const {token} = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,37 @@
+import {Alert} from 'react-native';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
+
+import api from '~/services/api';
+
+import {
+  updateProfileSuccess,
+  updateProfileFailure,
+} from '~/store/modules/user/actions';
+
+export function* updateProfile({payload}) {
+  try {
+    const {name, email, avatar_id, ...rest} = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    yield put(updateProfileSuccess(response.data));
+
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso.');
+  } catch (error) {
+    Alert.alert(
+      'Erro na atualização',
+      'Não foi possível atualizar seus dados.',
+    );
+
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
