1866767ba2db96a118511d25be8be1c48c06d521
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-05 21:08:41 -0300

    feat(Dashboard): add appoinments list and cancelation

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import api from '~/services/api';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+import {Container, Title, List} from './styles';
 
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map((appointment) => {
+        appointment.id === id
+          ? {...appointment, canceled_at: response.data.canceled_at}
+          : appointment;
+      }),
+    );
+  }
+
   return (
     <Background>
-      <Text>Agendamentos</Text>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({item}) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
+        />
+      </Container>
     </Background>
   );
 }
